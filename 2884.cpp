b96d999fdbe3e4ed377210aaab907434831f632c
성준모 <2021092379@hanyang.ac.kr>
2021-06-26 02:54:15 +0900

    baekjoon

---
+++ 2884.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int H, M;
+    cin >> H >> M;
+
+    if (M >= 45)
+    {
+        M -= 45;
+    }
+    else
+    {
+        M += 15;
+        if (H == 0)
+        {
+            H = 23;
+        }
+        else
+        {
+            H -= 1;
+        }
+    }
+    cout << H << " " << M;
+
+    return 0;
+}
