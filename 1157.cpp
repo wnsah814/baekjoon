4c8f50a9a55090808141473457e4cfb24faacd7d
성준모 <2021092379@hanyang.ac.kr>
2021-06-26 01:09:54 +0900

    baekjoon

---
+++ 1157.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+#include <utility>
+#include <string>
+using namespace std;
+
+vector<pair<int, string>> v;
+
+int main()
+{
+    string s;
+    cin >> s;
+
+    for (int i = 0; i < s.length(); i++)
+    {
+        if
+    }
+}
